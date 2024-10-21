919103cc2d9aadcf9890a3204ba84cd8ba76a798
Alegma <6178232+alegmal@users.noreply.github.com>
2024-10-22 01:54:29 +0300

    initial player placement

--- code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/App.jsx
+++ code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,10 +1,25 @@
-
 function App() {
-  
-
   return (
-    <p>Coming soonish...</p>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player">
+              <span className="player">Player 1</span>
+              <span className="player-symbol">X</span>
+            </span>
+          </li>
+          <li>
+            <span className="player">
+              <span className="player">Player 2</span>
+              <span className="player-symbol">O</span>
+            </span>
+          </li>
+        </ol>
+        GAME BOARD
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
