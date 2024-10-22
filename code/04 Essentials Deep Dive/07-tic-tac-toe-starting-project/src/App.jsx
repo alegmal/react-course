1657dec2bd4955ed9ce90708cfcb0e00ed0b9c54
Alegma <6178232+alegmal@users.noreply.github.com>
2024-10-23 01:15:01 +0300

    initial player name editing options

--- code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/App.jsx
+++ code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/App.jsx
@@ -5,11 +5,12 @@ function App() {
     <main>
       <div id="game-container">
         <ol id="players">
-          <Player name="Player 1" symbol="x"/>
-          <Player name="Player 2" symbol="o"/>
+          <Player name="Player 1" symbol="X" />
+          <Player name="Player 2" symbol="O" />
         </ol>
         GAME BOARD
       </div>
+      LOG
     </main>
   );
 }
--- code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,11 +1,30 @@
-export function Player({name, symbol}) {
+import { useState } from "react";
+
+export function Player({ name, symbol }) {
+  const [buttonText, setButtonText] = useState("Edit");
+  const [isEditing, setIsEditing] = useState(false);
+
+  const editHandler = () => {
+    if (!isEditing) {
+        setButtonText("Save");
+      setIsEditing(true);
+    } else {
+      setButtonText("Edit");
+      setIsEditing(false);
+    }
+  };
+
   return (
     <li>
       <span className="player">
-        <span className="player">{name}</span>
+        {isEditing ? (
+          <input type="text" required/>
+        ) : (
+          <span className="player-name">{name}</span>
+        )}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={editHandler}>{buttonText}</button>
     </li>
   );
 }
