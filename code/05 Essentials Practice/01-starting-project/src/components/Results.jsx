c6364f050be6942c5b23563b987e1bba6343f368
Alegma <6178232+alegmal@users.noreply.github.com>
2024-11-17 00:27:23 +0200

    init result implementation

--- code/05 Essentials Practice/01-starting-project/src/components/Results.jsx
+++ code/05 Essentials Practice/01-starting-project/src/components/Results.jsx
@@ -1,4 +1,7 @@
+import { calculateInvestmentResults } from "../util/investment";
+
 export default function Results({ userInput }) {
-  console.log(userInput);
+  const resultsData = calculateInvestmentResults(userInput)
+  console.log(resultsData)
   return <p>Results....</p>;
 }
